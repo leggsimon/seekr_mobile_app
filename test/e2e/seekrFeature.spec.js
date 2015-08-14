3995e368598c1b2f83c2b981081c3c7b0159bcdd
Simon Legg <leggsimon@gmail.com>
2015-08-14 09:26:14 +0100

    wage currency is in £s and adding jobs tests put in a describe

--- test/e2e/seekrFeature.spec.js
+++ test/e2e/seekrFeature.spec.js
@@ -1,19 +1,5 @@
 describe('Seekr', function() {
 
-  var addJobButton = element(by.id('addJob'));
-
-  var jobTitleForm = element(by.model('jobTitle'));
-  var jobDescriptionForm = element(by.model('jobDescription'));
-  var startDateForm = element(by.model('startDate'));
-  var durationForm = element(by.model('duration'));
-  var hoursForm = element(by.model('hours'));
-  var locationForm = element(by.model('location'));
-  var wageForm = element(by.model('wage'));
-  var requiredSkillsForm = element(by.model('requiredSkills'));
-
-  var submitButton = element(by.id('addJobSubmit'));
-
-
   beforeEach(function() {
     browser.get('http://localhost:8100');
   });
@@ -22,26 +8,43 @@ describe('Seekr', function() {
     expect(browser.getTitle()).toEqual('Jobs');
   });
 
-  it("has an 'add job' button", function() {
-    addJobButton.click();
-    expect(browser.getCurrentUrl()).toContain('/jobs/new');
+  describe('Creating jobs', function () {
+
+    var addJobButton = element(by.id('addJob'));
+
+    var jobTitleForm = element(by.model('jobTitle'));
+    var jobDescriptionForm = element(by.model('jobDescription'));
+    var startDateForm = element(by.model('startDate'));
+    var durationForm = element(by.model('duration'));
+    var hoursForm = element(by.model('hours'));
+    var locationForm = element(by.model('location'));
+    var wageForm = element(by.model('wage'));
+    var requiredSkillsForm = element(by.model('requiredSkills'));
+    var submitButton = element(by.id('addJobSubmit'));
+
+    it("has an 'add job' button", function() {
+      addJobButton.click();
+      expect(browser.getCurrentUrl()).toContain('/jobs/new');
+    });
+
+    it("allows jobs to be created and is viewed on /jobs", function() {
+      addJobButton.click();
+      jobTitleForm.sendKeys('nandos chef');
+      jobDescriptionForm.sendKeys('cooking chicken that is extra cheeky');
+      startDateForm.sendKeys('16/9/2015');
+      durationForm.sendKeys('3 months');
+      hoursForm.sendKeys('40');
+      locationForm.sendKeys('London');
+      wageForm.sendKeys('10 pounds an hour');
+      requiredSkillsForm.sendKeys('cooking');
+      submitButton.click();
+      expect(browser.getCurrentUrl()).toBe('http://localhost:8100/#/jobs');
+      var jobs = element.all(by.repeater('job in jobs'));
+      expect(jobs.last().getText()).toContain('nandos chef');
+    });
   });
 
-  it("allows jobs to be created", function() {
-    addJobButton.click();
-    jobTitleForm.sendKeys('nandos chef');
-    jobDescriptionForm.sendKeys('cooking chicken that is extra cheeky');
-    startDateForm.sendKeys('16/9/2015');
-    durationForm.sendKeys('3 months');
-    hoursForm.sendKeys('40');
-    locationForm.sendKeys('London');
-    wageForm.sendKeys('10 pounds an hour');
-    requiredSkillsForm.sendKeys('cooking');
-    submitButton.click();
-    expect(browser.getCurrentUrl()).toBe('http://localhost:8100/#/jobs');
-    var jobs = element.all(by.repeater('job in jobs'));
-    expect(jobs.last().getText()).toContain('nandos chef');
-  });
+
 
   // it('displays job-seekers', function() {
   //   var jobseekers = element.all(by.repeater('jobseeker in jobseekers'));
