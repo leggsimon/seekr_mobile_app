a11c0d58eaa3ad38c716819742f6b244586661d6
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-08-19 13:03:20 +0100

    refactoring jobCandidate controller

--- www/js/jobCandidatesController.js
+++ www/js/jobCandidatesController.js
@@ -2,8 +2,8 @@ seekr.controller('jobCandidatesCtrl', ['$scope', '$http', '$stateParams', 'Resta
   var api = Restangular.all('jobseekers');
   var url = 'http://tranquil-peak-9751.herokuapp.com/api';
 
-  var jobseekerData = api.getList().then(function(result){
-      $scope.jobseekers = result;
+  var jobseekerData = api.getList().then(function(result) {
+    $scope.jobseekers = result;
   });
 
 
@@ -15,25 +15,19 @@ seekr.controller('jobCandidatesCtrl', ['$scope', '$http', '$stateParams', 'Resta
 
   for (var i = 0; i < jobseekerData.length; i++) $scope.addCard();
 
-  $scope.cardSwipedLeft = function(id) {
-    reject(id);
-    console.log('Left swipe');
-  };
-
   $scope.cardSwipedRight = function(id) {
     $scope.jobseekers.push(angular.extend({}, id));
-    console.log('Right swipe');
-    console.log('Card put back in candidates');
+    console.log('Candidate kept');
   };
 
-  var reject = function(id) {
+  $scope.reject = function(id) {
     $http.post(url + '/offers', {
       'job_id': $scope.jobInfo.id,
       'job_seeker_id': id,
       'accepted': false,
-      }).success(function(data, status, headers, config) {
-        console.log('data inserted succesfully');
-      });
+    }).success(function(data, status, headers, config) {
+      console.log('Candidate rejected');
+    });
   };
 
   $scope.offer = function(id) {
@@ -41,9 +35,9 @@ seekr.controller('jobCandidatesCtrl', ['$scope', '$http', '$stateParams', 'Resta
       'job_id': $scope.jobInfo.id,
       'job_seeker_id': id,
       'accepted': true,
-      }).success(function(data, status, headers, config) {
-        console.log('data inserted succesfully');
-      });
+    }).success(function(data, status, headers, config) {
+      console.log('Candidate offered');
+    });
   };
 
 }]);
