c97f70e53706a299a391ff295343840bc48eb44f
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-08-19 12:59:39 +0100

    sending post requests for reject, offer, and shortlist

--- www/js/jobCandidatesController.js
+++ www/js/jobCandidatesController.js
@@ -1,5 +1,6 @@
-seekr.controller('jobCandidatesCtrl', ['$scope', '$stateParams', 'Restangular', function($scope, $stateParams, Restangular) {
+seekr.controller('jobCandidatesCtrl', ['$scope', '$http', '$stateParams', 'Restangular', function($scope, $http, $stateParams, Restangular) {
   var api = Restangular.all('jobseekers');
+  var url = 'http://tranquil-peak-9751.herokuapp.com/api';
 
   var jobseekerData = api.getList().then(function(result){
       $scope.jobseekers = result;
@@ -14,16 +15,35 @@ seekr.controller('jobCandidatesCtrl', ['$scope', '$stateParams', 'Restangular', 
 
   for (var i = 0; i < jobseekerData.length; i++) $scope.addCard();
 
-  $scope.cardSwipedLeft = function(index) {
+  $scope.cardSwipedLeft = function(id) {
+    reject(id);
     console.log('Left swipe');
   };
 
-  $scope.cardSwipedRight = function(index) {
+  $scope.cardSwipedRight = function(id) {
+    $scope.jobseekers.push(angular.extend({}, id));
     console.log('Right swipe');
+    console.log('Card put back in candidates');
   };
 
-  $scope.cardDestroyed = function(index) {
-    $scope.jobeekers.splice(index, 1);
-    console.log('Card removed');
+  var reject = function(id) {
+    $http.post(url + '/offers', {
+      'job_id': $scope.jobInfo.id,
+      'job_seeker_id': id,
+      'accepted': false,
+      }).success(function(data, status, headers, config) {
+        console.log('data inserted succesfully');
+      });
   };
+
+  $scope.offer = function(id) {
+    $http.post(url + '/offers', {
+      'job_id': $scope.jobInfo.id,
+      'job_seeker_id': id,
+      'accepted': true,
+      }).success(function(data, status, headers, config) {
+        console.log('data inserted succesfully');
+      });
+  };
+
 }]);
