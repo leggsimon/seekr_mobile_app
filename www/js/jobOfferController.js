8b2f4765c1242ec03d875125b698f314de174b02
Simon Legg <leggsimon@gmail.com>
2015-08-20 01:13:21 +0100

    map is button for blur toggle (in progress)

--- www/js/jobOfferController.js
+++ www/js/jobOfferController.js
@@ -1,6 +1,10 @@
 seekr.controller('jobOfferCtrl', ['$scope', '$stateParams', '$http', function ($scope, $stateParams, $http) {
 
   var url = 'http://tranquil-peak-9751.herokuapp.com/api';
+  
+  $scope.mapToggle = function() {
+    console.log('click')
+  };
 
   $http.get(url + '/jobseeker/' + [$stateParams.id] + '/offers').then(function(resp) {
     $scope.jobOffers = resp.data;
